e6676f0eea06db13fbed06c7885419857d7016eb
Christopher Amanor <kriskit7@gmail.com>
2020-09-27 19:42:49 +0000

    Connect component to redux store

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,17 +1,40 @@
-import React, { useState } from "react"
+// import React, { useState } from "react"
+// import Article from "../components/Article/Article"
+// import AddArticle from "../components/AddArticle/AddArticle"
+
+// const Articles = () => {
+//   const [articles, setArticles] = useState([
+//     { id: 1, title: "post 1", body: "Quisque cursus, metus vitae pharetra" },
+//     { id: 2, title: "post 2", body: "Quisque cursus, metus vitae pharetra" },
+//   ])
+//   const saveArticle = e => {
+//     e.preventDefault()
+//     // the logic will be updated later
+//   }
+
+//   return (
+//     <div>
+//       <AddArticle saveArticle={saveArticle} />
+//       {articles.map(article => (
+//         <Article key={article.id} article={article} />
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default Articles
+
+import React from "react"
+import { connect } from "react-redux"
+
 import Article from "../components/Article/Article"
 import AddArticle from "../components/AddArticle/AddArticle"
 
-const Articles = () => {
-  const [articles, setArticles] = useState([
-    { id: 1, title: "post 1", body: "Quisque cursus, metus vitae pharetra" },
-    { id: 2, title: "post 2", body: "Quisque cursus, metus vitae pharetra" },
-  ])
+const Articles = ({ articles }) => {
   const saveArticle = e => {
     e.preventDefault()
     // the logic will be updated later
   }
-
   return (
     <div>
       <AddArticle saveArticle={saveArticle} />
@@ -22,4 +45,10 @@ const Articles = () => {
   )
 }
 
-export default Articles
+const mapStateToProps = state => {
+  return {
+    articles: state.articles,
+  }
+}
+
+export default connect(mapStateToProps)(Articles)
