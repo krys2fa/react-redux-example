4d8bbf2dec387734f6ee3018dc0d5fd5fffb18f2
Christopher Amanor <kriskit7@gmail.com>
2020-09-27 20:07:17 +0000

    Update actionCreator to handle asynchronous code

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,35 +1,9 @@
-// import React, { useState } from "react"
-// import Article from "../components/Article/Article"
-// import AddArticle from "../components/AddArticle/AddArticle"
-
-// const Articles = () => {
-//   const [articles, setArticles] = useState([
-//     { id: 1, title: "post 1", body: "Quisque cursus, metus vitae pharetra" },
-//     { id: 2, title: "post 2", body: "Quisque cursus, metus vitae pharetra" },
-//   ])
-//   const saveArticle = e => {
-//     e.preventDefault()
-//     // the logic will be updated later
-//   }
-
-//   return (
-//     <div>
-//       <AddArticle saveArticle={saveArticle} />
-//       {articles.map(article => (
-//         <Article key={article.id} article={article} />
-//       ))}
-//     </div>
-//   )
-// }
-
-// export default Articles
-
 import React from "react"
 import { connect } from "react-redux"
 
 import Article from "../components/Article/Article"
 import AddArticle from "../components/AddArticle/AddArticle"
-import { addArticle } from "../store/actionCreators"
+import { simulateHttpRequest } from '../store/actionCreators';
 
 const Articles = ({ articles }) => {
   const saveArticle = e => {
@@ -54,8 +28,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    saveArticle: article => dispatch(addArticle(article)),
-  }
+    saveArticle: (article) => dispatch(simulateHttpRequest(article)),
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Articles)
